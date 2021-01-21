8af9552ba0b2d0023ac78db2c4753ef10d3f300b
Yurii Puchkov <panpuchkov@gmail.com>
2021-01-21 13:59:46 +0200

    MacOS - code signing

--- packaging/macos/qtappsign.sh
+++ packaging/macos/qtappsign.sh
@@ -4,7 +4,6 @@
 # https://forum.qt.io/topic/96652/how-to-notarize-qt-application-on-macos/18
 
 APP_NAME="${1}"
-QT_PATH="/usr/local/opt/qt5/bin"
 TEMP_CI_CERT_FILENAME="temp_ci_appleDistribution.p12"
 
 # Get the following variables from secrets via envs
@@ -15,8 +14,14 @@ TEMP_CI_CERT_FILENAME="temp_ci_appleDistribution.p12"
 # APPLE_DEVELOPER_ID_APPLICATION_CERT_DATA
 # APPLE_TEMP_CI_KEYCHAIN_PASS
 
+echo "*** TEMP_CI_CERT_FILENAME: $APPLE_TEMP_CI_KEYCHAIN_PASS"
+echo "*** APPLE_DEV_USER: ${APPLE_DEV_USER}"
+
 echo "--> Create key-chain and import certificate"
 
+# ***************************************************
+# ***** Create key-chain and import certificate *****
+# ***************************************************
 # create keychain
 security create-keychain -p "${APPLE_TEMP_CI_KEYCHAIN_PASS}" build.keychain
 security default-keychain -s build.keychain
@@ -30,15 +35,24 @@ security import "${TEMP_CI_CERT_FILENAME}" -P "${APPLE_DEVELOPER_ID_APPLICATION_
 security find-identity -v
 security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k "${APPLE_TEMP_CI_KEYCHAIN_PASS}" build.keychain
 
+# ****************************
+# ***** Sign application *****
+# ****************************
 echo "--> Start application signing process"
 codesign --sign "${APPLE_DEV_IDENTITY}" --verbose --deep ${APP_NAME}.app
 
+# *****************************
+# ***** Build dmg package *****
+# *****************************
 echo "--> Start packaging process"
-"${QT_PATH}/macdeployqt" "${APP_NAME}.app" -dmg -sign-for-notarization="${APPLE_DEV_IDENTITY}"
+"$(brew --prefix qt5)/bin/macdeployqt" "${APP_NAME}.app" -dmg -sign-for-notarization="${APPLE_DEV_IDENTITY}"
 
 #echo "--> Update dmg package links"
 #./{HELPERS_SCRIPTS_PATH}/update_package.sh
 
+# ********************************
+# ***** Notarize application *****
+# ********************************
 echo "--> Start Notarization process"
 response=$(xcrun altool -t osx -f "${APP_NAME}.dmg" --primary-bundle-id "org.namecheap.${APP_NAME}" --notarize-app -u "${APPLE_DEV_USER}" -p "${APPLE_DEV_PASS}")
 requestUUID=$(echo "${response}" | tr ' ' '\n' | tail -1)
@@ -52,7 +66,7 @@ while true; do
 
   if [[ "${isSuccess}" != "" ]]; then
     echo "Notarization done!"
-    xcrun stapler staple -v "${APP_NAME}.dmg"
+    xcrun stapler staple "${APP_NAME}.dmg"
     echo "Stapler done!"
     break
   fi
@@ -65,8 +79,14 @@ while true; do
   sleep 120
 done
 
+# ****************************
+# ***** Sign dmg package *****
+# ****************************
 echo "--> Start dmg signing process"
 codesign --sign "${APPLE_DEV_IDENTITY}" --verbose --deep "${APP_NAME}.dmg"
 
+# ***********************************
+# ***** Verify dmg package sign *****
+# ***********************************
 echo "--> Start verify signing process"
 codesign -dv --verbose=4 "${APP_NAME}.dmg"
