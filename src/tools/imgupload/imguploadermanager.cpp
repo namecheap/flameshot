6a64e1817e9ae77b6c5be2fd762597adba449782
Yuriy Puchkov <yuriy.puchkov@namecheap.com>
2025-08-14 14:02:22 -0600

    feat: upload to the s3-bucket for the Namecheap

--- src/tools/imgupload/imguploadermanager.cpp
+++ src/tools/imgupload/imguploadermanager.cpp
@@ -9,6 +9,8 @@
 // TODO - remove this hard-code and create plugin manager in the future, you may
 // include other storage headers here
 #include "storages/imgur/imguruploader.h"
+#include "storages/s3/imgs3settings.h"
+#include "storages/s3/imgs3uploader.h"
 
 ImgUploaderManager::ImgUploaderManager(QObject* parent)
   : QObject(parent)
@@ -23,14 +25,15 @@ void ImgUploaderManager::init()
 {
     // TODO - implement ImgUploader for other Storages and selection among them,
     // example:
-    // if (uploaderPlugin().compare("s3") == 0) {
-    //    m_qstrUrl = ImgS3Settings().value("S3", "S3_URL").toString();
-    //} else {
-    //    m_qstrUrl = "https://imgur.com/";
-    //    m_imgUploaderPlugin = "imgur";
-    //}
-    m_urlString = "https://imgur.com/";
-    m_imgUploaderPlugin = "imgur";
+    if (uploaderPlugin().compare("s3") == 0) {
+        m_urlString = ImgS3Settings().value("S3", "S3_URL").toString();
+        m_imgUploaderPlugin = "s3";
+    } else {
+        m_urlString = "https://imgur.com/";
+        m_imgUploaderPlugin = "imgur";
+    }
+    // m_urlString = "https://imgur.com/";
+    // m_imgUploaderPlugin = "imgur";
 }
 
 ImgUploaderBase* ImgUploaderManager::uploader(const QPixmap& capture,
@@ -38,14 +41,15 @@ ImgUploaderBase* ImgUploaderManager::uploader(const QPixmap& capture,
 {
     // TODO - implement ImgUploader for other Storages and selection among them,
     // example:
-    // if (uploaderPlugin().compare("s3") == 0) {
-    //    m_imgUploaderBase =
-    //      (ImgUploaderBase*)(new ImgS3Uploader(capture, parent));
-    //} else {
-    //    m_imgUploaderBase =
-    //      (ImgUploaderBase*)(new ImgurUploader(capture, parent));
-    //}
-    m_imgUploaderBase = (ImgUploaderBase*)(new ImgurUploader(capture, parent));
+    if (uploaderPlugin().compare("s3") == 0) {
+        m_imgUploaderBase =
+          (ImgUploaderBase*)(new ImgS3Uploader(capture, parent));
+    } else {
+        m_imgUploaderBase =
+          (ImgUploaderBase*)(new ImgurUploader(capture, parent));
+    }
+    // m_imgUploaderBase = (ImgUploaderBase*)(new ImgurUploader(capture,
+    // parent));
     if (m_imgUploaderBase && !capture.isNull()) {
         m_imgUploaderBase->upload();
     }
